90657da83c62d16f854b9d4d45b26a717676386e
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-08-09 09:56:27 +0300

    Display search results

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
-const SearchPage = ({toggleSearchState}) => (
-  <div className="search-books">
-    <div className="search-books-bar">
-      <Link
-        className="close-search"
-        to="/">Close</Link>
-      <div className="search-books-input-wrapper">
-        {/*
+import {search} from '../utils/BooksAPI';
+import BookShelf from '../components/BookShelf';
+
+class SearchPage extends Component {
+  state = {
+    results: []
+  };
+
+  onInputChange({ target: { value }}) {
+    search(value)
+      .then((results) => this.setState({results: Array.isArray(results) ? results : []}));
+  }
+
+  render() {
+    const books = this.state.results;
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
           NOTES: The search from BooksAPI is limited to a particular set of search terms.
           You can find these search terms here:
           https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -16,14 +30,16 @@ const SearchPage = ({toggleSearchState}) => (
           However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
           you don't find a specific author or title. Every search is limited by search terms.
         */}
-        <input type="text" placeholder="Search by title or author"/>
-
+            <input type="text" placeholder="Search by title or author" onChange={this.onInputChange.bind(this)} />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+          <BookShelf title={'Search results'} books={books}/>
+        </div>
       </div>
-    </div>
-    <div className="search-books-results">
-      <ol className="books-grid"></ol>
-    </div>
-  </div>
-);
+    );
+  }
+};
 
 export default SearchPage
