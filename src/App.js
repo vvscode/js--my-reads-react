015ffe9c555dd09b8a9392e23eeb9f3a4da42344
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-08-11 17:46:07 +0300

    Improve responsibility. Extract util function to make complexity less

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import SearchPage from './pages/SearchPage';
 import BooksListPage from './pages/BooksListPage';
 import {update, getAll} from './utils/BooksAPI';
+import updateListById from './utils/updateListById';
 
 // todo: remove this
 window.BooksAPI = BooksAPI;
@@ -15,34 +16,36 @@ class BooksApp extends React.Component {
     books: []
   };
 
-  constructor(...args) {
-    super(...args);
-    this.updateBookShelf = this.updateBookShelf.bind(this);
-  }
+  // constructor(...args) {   super(...args);   this.updateBookShelf = this
+  // .updateBookShelf     .bind(this); }
 
   componentWillMount() {
     getAll().then((books) => this.setState({books}));
   }
 
-  updateBookShelf(book, shelf) {
-    update(book, shelf).then(() => this.setState({
-      books: this
-        .state
-        .books
-        .map(item => item.id === book.id
-          ? ({
-            ...book,
-            shelf
-          })
-          : item)
-    }))
+  updateBookShelf = (book, shelf) => {
+    const oldShelf = book.shelf;
+
+    // positive expectations for UI to improve feedback speed
+    // update shelf in time, on error - do rallback
+    this.setState({
+      books: updateListById(this.state.books, book.id, {shelf})
+    });
+    update(book, shelf).catch(() => this.setState({
+      books: updateListById(this.state.books, book.id, {shelf: oldShelf})
+    }));
   }
 
   render() {
     return (
       <div className="app">
-        <Route exact path='/' render={() => <BooksListPage books={this.state.books} updateBookShelf={this.updateBookShelf} />}/>
-        <Route path='/search' render={() => <SearchPage books={this.state.books} updateBookShelf={this.updateBookShelf} />}/>
+        <Route
+          exact
+          path='/'
+          render={() => <BooksListPage books={this.state.books} updateBookShelf={this.updateBookShelf}/>}/>
+        <Route
+          path='/search'
+          render={() => <SearchPage books={this.state.books} updateBookShelf={this.updateBookShelf}/>}/>
       </div>
     )
   }
---
+++ src/utils/updateListById.js
@@ -0,0 +1,8 @@
+/**
+ * Generate new list
+ * with the only object matched by id will be updated with props
+ */
+export default (list, id, props) => list.map((item) => item.id !== id ? item : {
+  ...item,
+  ...props
+});
