0bfc1595c404b32a838d344e3696719091c22ca2
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-11-21 14:21:53 +0300

    Reformat code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import * as BooksAPI from './utils/BooksAPI';
-import {Route} from 'react-router-dom';
+import React from "react";
+import * as BooksAPI from "./utils/BooksAPI";
+import { Route } from "react-router-dom";
 
-import './App.css';
-import SearchPage from './pages/SearchPage';
-import BooksListPage from './pages/BooksListPage';
-import {update, getAll} from './utils/BooksAPI';
-import updateListById from './utils/updateListById';
+import "./App.css";
+import SearchPage from "./pages/SearchPage";
+import BooksListPage from "./pages/BooksListPage";
+import { update, getAll } from "./utils/BooksAPI";
+import updateListById from "./utils/updateListById";
 
 // todo: remove this
 window.BooksAPI = BooksAPI;
@@ -20,7 +20,7 @@ class BooksApp extends React.Component {
   // .updateBookShelf     .bind(this); }
 
   componentWillMount() {
-    getAll().then((books) => this.setState({books}));
+    getAll().then(books => this.setState({ books }));
   }
 
   updateBookShelf = (book, shelf) => {
@@ -29,26 +29,40 @@ class BooksApp extends React.Component {
     // positive expectations for UI to improve feedback speed
     // update shelf in time, on error - do rallback
     this.setState({
-      books: updateListById(this.state.books, book.id, {shelf})
+      books: updateListById(this.state.books, book.id, { shelf })
     });
-    update(book, shelf).catch(() => this.setState({
-      books: updateListById(this.state.books, book.id, {shelf: oldShelf})
-    }));
-  }
+    update(book, shelf).catch(() =>
+      this.setState({
+        books: updateListById(this.state.books, book.id, { shelf: oldShelf })
+      })
+    );
+  };
 
   render() {
     return (
       <div className="app">
         <Route
           exact
-          path='/'
-          render={() => <BooksListPage books={this.state.books} updateBookShelf={this.updateBookShelf}/>}/>
+          path="/"
+          render={() => (
+            <BooksListPage
+              books={this.state.books}
+              updateBookShelf={this.updateBookShelf}
+            />
+          )}
+        />
         <Route
-          path='/search'
-          render={() => <SearchPage books={this.state.books} updateBookShelf={this.updateBookShelf}/>}/>
+          path="/search"
+          render={() => (
+            <SearchPage
+              books={this.state.books}
+              updateBookShelf={this.updateBookShelf}
+            />
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/utils/updateListById.js
+++ src/utils/updateListById.js
@@ -2,7 +2,13 @@
  * Generate new list
  * with the only object matched by id will be updated with props
  */
-export default (list, id, props) => list.map((item) => item.id !== id ? item : {
-  ...item,
-  ...props
-});
+export default (list, id, props) =>
+  list.map(
+    item =>
+      item.id !== id
+        ? item
+        : {
+            ...item,
+            ...props
+          }
+  );
